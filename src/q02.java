ba00ae3f4823bfddb8e8badccc46a1ec5b24db5d
kavindus0 <kavindusachinthe@outlook.com>
2024-12-16 11:29:41 +0530

    Create q02.java

---
+++ src/q02.java
@@ -0,0 +1,23 @@
+public class q02 {
+    //Write a Java program that prompts the user to enter the number of elements in an array
+    // and then input the elements of the array.
+    // The program should call a method named minGap,
+    // which accepts the array as a parameter and
+    // calculates the minimum gap between adjacent elements,
+    // defined as the absolute difference between consecutive elements.
+    // If the array contains fewer than 2 elements, the method should return 0.
+    // The main method should display the result returned by minGap.
+    // For example, if the user enters the array {1, 3, 6, 5, 12},
+    // the gaps are 2 (3 - 1), 3 (6 - 3), 1 (6 - 5), and 7 (12 - 5),
+    // so minGap should return 1.
+
+    int minGap(int[] num){
+        if(num.length<2){
+            return 0;
+        }
+
+        return 0;
+
+    }
+
+}
